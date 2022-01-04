076da84fe1789d5f5355b16537fd04301231b074
ChoiDongJoon <adong0808@naver.com>
2022-01-04 22:32:53 +0900

    2022/01/04

---
+++ practice.py
@@ -0,0 +1,22 @@
+a = 2
+b = 3
+c = False
+print(a + b)
+d = None
+
+day_list = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+print(type(day_list))
+print(day_list[3])
+day_list.append("Sat")
+print(day_list)
+day_tuple = ("Mon", "Tue", "Wed", "Thur", "Fri")
+print(type(day_tuple))
+
+dongjun = {
+    "name" : "dongjun",
+    "age" : 23,
+    "korean" : True
+    }
+print(dongjun)
+dongjun["handsome"] = True
+print(dongjun)
