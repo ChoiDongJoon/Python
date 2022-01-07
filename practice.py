4337e2b5aaf3f3a3572d3126785f3097813407b5
ChoiDongJoon <adong0808@naver.com>
2022-01-07 22:32:16 +0900

    2022/01/07

--- practice.py
+++ practice.py
@@ -30,3 +30,42 @@ def say(name, age) :
 hello = say("dongjun", "23")
 print(hello)
 
+def plus(a, b):
+    if type(b) is int or type(b) is float:
+        return a+b
+    else:
+        return None
+print(plus(12, 1.2))
+print(plus(12, "1.2"))
+
+def age_check(age):
+    print(f"you are {age}")
+    if age < 18:
+        print("you cant drink")
+    elif age == 18:
+        print("you are new to this!")
+    else:
+        print("enjoy your drink")
+
+age_check(19)
+age_check(18)
+age_check(17)
+
+
+days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+
+for day in days:
+    print(day)
+
+import math
+
+print(math.ceil(1.2))
+print(math.fabs(-1.2))
+
+from math import ceil, fsum
+
+
+
+
+
+
